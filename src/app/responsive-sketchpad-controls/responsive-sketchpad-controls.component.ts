f65aebd3022881c8c74588fa9c7e804144e7f896
Jonas Wyss <jonaswyss@gmail.com>
2017-04-29 21:23:41 +0200

    add controls for sketchpad-controls

--- src/app/responsive-sketchpad-controls/responsive-sketchpad-controls.component.ts
+++ src/app/responsive-sketchpad-controls/responsive-sketchpad-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-responsive-sketchpad-controls',
@@ -7,21 +7,21 @@ import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 })
 export class ResponsiveSketchpadControlsComponent implements OnInit {
 
-
+  public colors: Array<string> = new Array<string>("red", "blue", "green", "yellow", "black", "white");
 
   @Output() lineColorUpdated = new EventEmitter<string>();
   @Output() lineSizeUpdated = new EventEmitter<number>();
-  @Output() unDo= new EventEmitter();
+  @Output() unDo = new EventEmitter();
   @Output() clear = new EventEmitter();
 
-  //Fields
-  private _lineColor: string ;
-  private _lineSize: number ;
-  //Properties
-  get lineColor():string {
+  // Fields
+  private _lineColor: string;
+  private _lineSize: number;
+  // Properties
+  get lineColor(): string {
     return this._lineColor;
   }
-  set lineColor(value:string){
+  set lineColor(value: string) {
     this._lineColor = value;
     this.lineColorUpdated.emit(this.lineColor);
   }
@@ -34,22 +34,35 @@ export class ResponsiveSketchpadControlsComponent implements OnInit {
     this.lineSizeUpdated.emit(this.lineSize);
   }
 
-  unDoClicked()
-  {
+  unDoClicked() {
     console.log("undo clicked");
     this.unDo.emit();
   }
 
-  clearClicked()
-  {
+  clearClicked() {
     this.clear.emit();
   }
 
   constructor() {
-    
-   }
+    this.colors.forEach(color => {
 
+    })
+  }
+  setColor(color: string) {
+    this.lineColor = color;
+  }
+  decreasePenSize() {
+    this.lineSize -= 2;
+    if(this.lineSize<2)
+    this.lineSize=2;
+  }
+  increasePenSize() {
+    this.lineSize += 2;
+    if(this.lineSize>100)
+        this.lineSize=100;
+  }
   ngOnInit() {
+
     //set default;
     this.lineSize = 5;
     this.lineColor = "black";
