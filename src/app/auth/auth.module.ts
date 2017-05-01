00aa27e1a2ba5dd9fef6fd132a33297b7c7ba3e5
Fabio Zuber <fabio.zuber92@gmail.com>
2017-05-01 22:59:52 +0200

    feature(routing): start with bottom nav bar

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
-import {AuthGuard} from './services/auth-guard.service';
 import {LoginEmailComponent} from './components/login-email.component';
 import {LoginComponent} from './components/login.component';
 import {RegisterEmailComponent} from './components/register-email.component';
+import {AuthGuard} from './services';
 
 @NgModule({
   imports: [
