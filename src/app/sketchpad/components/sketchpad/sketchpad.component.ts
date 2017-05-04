f569c192206364eeb5a4b0e7fe5fd06fb1de5a68
Jonas Wyss <jonaswyss@gmail.com>
2017-05-04 11:15:01 +0200

    rewrite basic sketchpad functions (working)

--- src/app/sketchpad/components/sketchpad/sketchpad.component.ts
+++ src/app/sketchpad/components/sketchpad/sketchpad.component.ts
@@ -18,14 +18,43 @@ export class SketchpadComponent implements OnInit {
   private mainCanvasContext: CanvasRenderingContext2D;
   private topCanvasContext: CanvasRenderingContext2D;
 
-  private lineWidth: number;
-  private lineColor: string;
 
-  private paths: Array<Path>;
+
+  private pathsRelative: Array<Path> = new Array<Path>();
+  private pathsAbsolute: Array<Path> = new Array<Path>();
+  private activePathAbsolute: Path;
+
+  private distanceTop: number;
+  private distanceLeft: number;
+
+  private _isDrawing: boolean;
   /**** Properties ****/
   public get canvas(): HTMLCanvasElement {
     return this.topCanvas;
   }
+
+  public canvasHeight: number;
+  public canvasWidth: number;
+
+  public lineWidth: number;
+  public lineColor: string;
+
+  /** Private Properties */
+
+
+  private get IsDrawing(): boolean {
+    return this._isDrawing;
+  }
+  private set IsDrawing(value: boolean) {
+    if (value && !this._isDrawing) {
+      this.StartNewPath();
+    }
+    if (!value && this._isDrawing) {
+      this.FinishPath();
+    }
+    this._isDrawing = value;
+  }
+
   /**** InputEvents ****/
   private mouseup = new EventEmitter<MouseEvent>();
   private mousemove = new EventEmitter<MouseEvent>();
@@ -60,36 +89,49 @@ export class SketchpadComponent implements OnInit {
   @HostListener('touchmove', ['$event'])
   onTouchMove(event: TouchEvent) { event.preventDefault(); this.touchMove.emit(event); }
 
+  onResize(event) { this.SetCanvasSize(); }
 
   ngOnInit(): void {
 
 
     this.mouseup.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = false; }
     });
     this.mouseleave.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = false; }
     });
     this.mousemove.subscribe({
-      next: event => { }
+      next: event => {
+        if (this.IsDrawing) {
+          this.AddPointToActivePath(this.GetCoordsFromMouseEvent(event));
+        };
+      }
     });
     this.mousedown.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = true; }
     });
 
     this.touchEnd.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = false; }
     });
     this.touchCancle.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = false; }
     });
     this.touchMove.subscribe({
-      next: event => { }
+      next: event => { this.AddPointToActivePath(this.GetCoordsFromTouchEvent(event)) }
     });
     this.touchStart.subscribe({
-      next: event => { }
+      next: event => { this.IsDrawing = true; }
     });
+    this.topCanvas = this.topCanvasRef.nativeElement;
+    this.mainCanvas = this.mainCanvasRef.nativeElement;
 
+    this.mainCanvasContext = this.mainCanvas.getContext('2d');
+    this.topCanvasContext = this.topCanvas.getContext('2d');
+
+    this.SetCanvasSize();
+
+    this.SetCanvasDistance();
 
 
   }
@@ -110,7 +152,61 @@ export class SketchpadComponent implements OnInit {
     this.lineWidth = lineSize;
   }
 
+  public Clear() {
+    this.pathsRelative = new Array<Path>();
+    this.pathsAbsolute = new Array<Path>();
+    this.ClearCanvas(this.mainCanvasContext);
+  }
+  public UnDo() {
+    this.pathsRelative.splice(-1, 1);
+    this.pathsAbsolute.splice(-1, 1);
+    this.RedrawCanvas(this.mainCanvasContext, this.pathsAbsolute);
+  }
+
   /**** Private Functions ****/
+  private StartNewPath() {
+    this.activePathAbsolute = new Path(this.lineWidth, this.lineColor);
+    console.log("Start new Path");
+  }
+
+  private AddPointToActivePath(coord: Coord) {
+
+    this.activePathAbsolute.Strokes.push(new Stroke(coord));
+    this.RedrawCanvas(this.topCanvasContext, [this.activePathAbsolute]);
+  }
+
+  private FinishPath() {
+    this.pathsAbsolute.push(this.activePathAbsolute);
+    this.pathsRelative.push(this.GetRelativePath(this.activePathAbsolute, this.canvasWidth, this.canvasHeight));
+    this.DrawPathToCanvas(this.mainCanvasContext, this.activePathAbsolute);
+    this.ClearCanvas(this.topCanvasContext);
+
+  }
+
+  private GetCoordsFromMouseEvent(event: MouseEvent) {
+    const y: number = event.pageY - this.distanceTop;
+    const x: number = event.pageX - this.distanceLeft;
+    return new Coord(x, y);
+  }
+
+  private GetCoordsFromTouchEvent(event: TouchEvent) {
+    const y: number = event.changedTouches[0].pageY - this.distanceTop;
+    const x: number = event.changedTouches[0].pageX - this.distanceLeft;
+    return new Coord(x, y);
+  }
+
+
+  private SetCanvasSize() {
+    this.canvasHeight = this.mainCanvas.clientHeight;
+    this.canvasWidth = this.mainCanvas.clientWidth;
+  }
+
+  private SetCanvasDistance() {
+    this.distanceTop = this.canvas.getBoundingClientRect().top;
+    this.distanceLeft = this.canvas.getBoundingClientRect().left;
+  }
+
+
   private DrawPathToCanvas(canvasContext: CanvasRenderingContext2D, path: Path) {
     canvasContext.beginPath();
     // start at first point of path
@@ -124,7 +220,27 @@ export class SketchpadComponent implements OnInit {
       // move to current point
       canvasContext.moveTo(stroke.coord.x, stroke.coord.y);
     });
+    canvasContext.strokeStyle = path.lineColor;
+    canvasContext.lineWidth = path.lineWidth;
+    canvasContext.lineCap = "round";
+    canvasContext.stroke();
   }
+
+  private RedrawCanvas(canvasContext: CanvasRenderingContext2D, paths: Array<Path>) {
+    this.ClearCanvas(canvasContext);
+    paths.forEach(path => {
+      this.DrawPathToCanvas(canvasContext, path);
+    });
+  }
+
+
+  private ClearCanvas(canvasContext: CanvasRenderingContext2D) {
+    const canvasHeight: number = canvasContext.canvas.height;
+    const canvasWidth: number = canvasContext.canvas.width;
+    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  }
+
 
   private GetAbsolutePathArray(paths: Array<Path>, xFactor: number, yFactor: number): Array<Path> {
     const newPaths: Array<Path> = new Array<Path>();
@@ -134,7 +250,7 @@ export class SketchpadComponent implements OnInit {
 
     return newPaths;
   }
-  public GetAbsolutePath(path: Path, xFactor: number, yFactor: number): Path {
+  private GetAbsolutePath(path: Path, xFactor: number, yFactor: number): Path {
     const newPath: Path = new Path(path.lineWidth, path.lineColor);
     path.Strokes.forEach(stroke => {
       newPath.Strokes.push(this.GetAbsoluteStroke(stroke, xFactor, yFactor));
@@ -142,10 +258,13 @@ export class SketchpadComponent implements OnInit {
 
     return newPath;
   }
-  public GetAbsoluteStroke(stroke:Stroke, xFactor: number, yFactor: number):Stroke {
-
+  private GetAbsoluteStroke(stroke: Stroke, xFactor: number, yFactor: number): Stroke {
+    const newX: number = stroke.coord.x * xFactor;
+    const newY: number = stroke.coord.y * yFactor;
+    const newCoord: Coord = new Coord(newX, newY);
+    const newStroke: Stroke = new Stroke(newCoord);
+    return newStroke;
   }
-
   private GetRelativePathArray(paths: Array<Path>, xDivider: number, yDivider: number): Array<Path> {
     const newPaths: Array<Path> = new Array<Path>();
     paths.forEach(path => {
@@ -154,13 +273,23 @@ export class SketchpadComponent implements OnInit {
 
     return newPaths;
   }
-  public GetRelativePath(path: Path, xDivider: number, yDivider: number): Path {
+  private GetRelativePath(path: Path, xDivider: number, yDivider: number): Path {
+    const newPath: Path = new Path(path.lineWidth, path.lineColor);
+    path.Strokes.forEach(stroke => {
+      newPath.Strokes.push(this.GetRelativeStroke(stroke, xDivider, yDivider));
+    });
 
+    return newPath;
   }
-  public GetRelativeStroke(stroke:Stroke, xDivider: number, yDivider: number):Stroke {
-    
+  private GetRelativeStroke(stroke: Stroke, xDivider: number, yDivider: number): Stroke {
+    const newX: number = stroke.coord.x / xDivider;
+    const newY: number = stroke.coord.y / yDivider;
+    const newCoord: Coord = new Coord(newX, newY);
+    const newStroke: Stroke = new Stroke(newCoord);
+    return newStroke;
   }
 
+  /*** Public Functions **/
 
 
 
@@ -317,7 +446,7 @@ export class SketchpadComponent implements OnInit {
     this.lineColor = color;
   }
 
-  public SetLineSize(size: number) {
+  public (size: number) {
     this.lineSize = size;
   }
 
