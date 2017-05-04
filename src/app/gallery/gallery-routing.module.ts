699a3057708ea9c11cbf8e0c0485c14d5092d54e
Fabio Zuber <fabio.zuber92@gmail.com>
2017-05-04 16:01:44 +0200

    feat(gallery): add empty topic component

--- src/app/gallery/gallery-routing.module.ts
+++ src/app/gallery/gallery-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TopicListComponent} from "./topic-list/topic-list.component";
 import {AuthGuard} from "../auth/services/auth-guard.service";
+import {TopicComponent} from "./topic/topic.component";
 
 const galleryRoutes: Routes = [
   { path: 'gallery',  redirectTo: 'gallery/topics' },
   { path: 'gallery/topics',  component: TopicListComponent, canActivate: [AuthGuard] },
-  { path: 'gallery/topic/:id', component: TopicListComponent, canActivate: [AuthGuard] } // TODO change to actual component
+  { path: 'gallery/topic/:id', component: TopicComponent, canActivate: [AuthGuard] } // TODO change to actual component
 ];
 @NgModule({
   imports: [
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopicListComponent } from './topic-list/topic-list.component';
 import {GalleryRoutingModule} from "./gallery-routing.module";
+import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GalleryRoutingModule
   ],
-  declarations: [TopicListComponent]
+  declarations: [TopicListComponent, TopicComponent]
 })
 export class GalleryModule { }
