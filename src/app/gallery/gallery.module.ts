64f1bd5672cd654b01a09ee78a4f194577481160
Fabio Zuber <fabio.zuber92@gmail.com>
2017-05-04 14:59:53 +0200

    feat(gallery): add empty module and component for gallery

---
+++ src/app/gallery/gallery.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopicListComponent } from './topic-list/topic-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TopicListComponent]
+})
+export class GalleryModule { }
