630c4549c91f2b8ff136ffc0368da79c3e0ad974
Fabio Zuber <fabio.zuber92@gmail.com>
2017-05-07 14:23:14 +0200

    feature(layout): move top menu into angular part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private location: Location) { }
+
+  goback() {
+    this.location.back();
+  }
 
 }
