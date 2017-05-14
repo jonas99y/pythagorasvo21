40e616500108266a52972a692ee402cbedb10560
Fabio Zuber <fabio.zuber92@gmail.com>
2017-05-14 17:23:08 +0200

    fix(firebase): enable cors on server side

--- functions/src/register-user/index.ts
+++ functions/src/register-user/index.ts
@@ -2,14 +2,27 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 export const listener = functions.https.onRequest((req, res) => {
-    const firstname =  req.body.firstname;
-    const lastname =  req.body.lastname;
+    res.set('Access-Control-Allow-Origin', '*')
+    .set('Access-Control-Allow-Methods', 'GET, POST')
+    .status(200)
+  if (req.method === `OPTIONS`) {
+    res.send('options requested');
+    return;
+  } else {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
     const username = req.body.username;
     const uid = req.body.uid;
-    const snapshot = admin.database().ref('users').push({ username: username, firstname: firstname, lastname: lastname, uid: uid });
-    const key =snapshot.key;
+    const snapshot = admin.database().ref('users').push({
+      username: username,
+      firstname: firstname,
+      lastname: lastname,
+      uid: uid
+    });
+    const key = snapshot.key;
     const updates = {};
-    updates[uid]=key;
+    updates[uid] = key;
     admin.database().ref('userUIDs').update(updates);
-
+    res.send('wheeee it works!');
+  }
 });
