5dbc0488aec851d6d6ca4614b798a5811d261dcc
Jonas Wyss <jonaswyss@gmail.com>
2017-05-29 23:00:08 +0200

    function no loger sets lastname as "not set"

--- functions/src/on-new-user/index.ts
+++ functions/src/on-new-user/index.ts
@@ -3,11 +3,9 @@ import * as admin from 'firebase-admin';
 
 export const onNewUser = functions.auth.user().onCreate(event => {
     let firstname = event.data.displayName;
-    let lastname = "not set";
     let uid =event.data.uid;
     const snapshot = admin.database().ref('users').push({
         firstname: firstname,
-        lastname: lastname,
         uid: uid,
         images: admin.database().ref().push().key
     });
