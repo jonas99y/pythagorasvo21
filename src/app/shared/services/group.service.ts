cab383b2efc7e00a58bfb83b37b79167a05226f1
Jonas Wyss <jonaswyss@gmail.com>
2017-05-30 11:33:11 +0200

    implement basic group service

--- src/app/shared/services/group.service.ts
+++ src/app/shared/services/group.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { DBHelperService } from './db-helper.service';
+import { Group, User } from '../models';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class GroupService {
 
-  constructor() { }
+  constructor(private dbHelperService: DBHelperService) { }
 
+
+  findGroupAfterKey(key: string): FirebaseObjectObservable<Group> {
+    return this.dbHelperService.findInNodeAfterKey("groups", key);
+  }
+
+  findAllUsersInGroup(group: FirebaseObjectObservable<Group>): Promise<FirebaseListObservable<User>> {
+    const promise = new Promise((resolve, reject) => {
+      group.subscribe(groupSnapshot => {
+        resolve(this.dbHelperService.findKeyList(groupSnapshot.users));
+      });
+    });
+    return promise;
+  }
+
+  addUserToGroup(group: FirebaseObjectObservable<Group>, user: FirebaseObjectObservable<Group>): Promise<any> {
+    const promise = new Promise((resolve, reject) => {
+      group.subscribe(groupSnapshot => {
+        this.dbHelperService.addKeyToList(groupSnapshot.users, user.$ref.key);
+        resolve();
+      });
+    });
+    return promise;
+  }
 }
