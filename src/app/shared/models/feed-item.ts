c848730a0507307ceac2b1531e65d75158f7072c
Jonas Wyss <jonaswyss@gmail.com>
2017-06-14 16:46:38 +0200

    add feed-item to model

---
+++ src/app/shared/models/feed-item.ts
@@ -0,0 +1,11 @@
+export class FeedItem {
+    constructor(
+        public author: string,
+        public comments: string,
+        public rating: string,
+        public timestamp: string,
+        public image: string,
+        public message: string
+    ) {
+    }
+};
