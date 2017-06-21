21622b49ff92a015adaa70564828ec34ff492835
Jonas Wyss <jonaswyss@gmail.com>
2017-06-21 15:05:51 +0200

    draw-request-item has how working link to '/draw/topic'

--- src/app/feed/components/draw-request-item/draw-request-item.component.ts
+++ src/app/feed/components/draw-request-item/draw-request-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Injector,Input } from '@angular/core';
-import { Topic} from '../../../shared';
-import {FirebaseObjectObservable} from 'angularfire2/database';
+import { Component, OnInit, Injector, Input } from '@angular/core';
+import { Topic } from '../../../shared';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-draw-request-item',
@@ -11,12 +11,16 @@ export class DrawRequestItemComponent implements OnInit {
 
   @Input() topic: FirebaseObjectObservable<Topic>;
 
+  public Topic: Topic;
 
   constructor(public injector: Injector) {
     this.topic = this.injector.get('topic');
   }
 
   ngOnInit() {
+    this.topic.subscribe(topicSnapshot => {
+      this.Topic = topicSnapshot;
+    })
   }
 
 }
