155f776601fdb870e098c02adb50db746ef0d1eb
Jonas Wyss <jonaswyss@gmail.com>
2017-06-28 12:12:57 +0200

    show message when feed is empty

--- src/app/feed/components/feed/feed.component.ts
+++ src/app/feed/components/feed/feed.component.ts
@@ -14,7 +14,7 @@ export class FeedComponent {
   @Input() fields: Array<any> = new Array<any>();
   @Input() set feedItems(feedItems: Observable<Array<FirebaseObjectObservable<FeedItem>>>) {
     if (feedItems !== undefined) {
-      console.log("sub");
+
       feedItems.subscribe(feedItemsSnapshot => {
         this.fields.splice(0, this.fields.length );
         console.log(this.fields.length);
@@ -23,7 +23,6 @@ export class FeedComponent {
           feedItem => {
             this.feedItemResolverService.setFeedItem(feedItem).then(item => {
               this.fields.push(item);
-              console.log(item);
             });
           }
         );
